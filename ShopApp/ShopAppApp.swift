d1e9b636877a2b3b1ed38bfb40fe88b62db596eb
Rei Soemanto <reresoemanto@gmail.com>
2026-05-13 17:07:25 +0700

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Rei Soemanto on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopAppTests/ShopAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppTests.swift
+//  ShopAppTests
+//
+//  Created by Rei Soemanto on 13/05/26.
+//
+
+import Testing
+@testable import ShopApp
+
+struct ShopAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
